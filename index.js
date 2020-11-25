05722b4d49fe5ec13ff435a5158b61216662ac3d
Lucah Endicott <lucahdavid88@gmail.com>
2020-11-24 16:18:34 -0800

    add inquirer prompts

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const inquirer = require("inquirer")
 const fs = require("fs")
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "titleInput",
+    },
+    {
+        type: "input",
+        message: "Enter application installtion instructions.",
+        name: "installInput",
+    },
+    {
+        type: "input",
+        message: "Enter application usage information.",
+        name: "usageInput",
+    },
+    {
+        type: "list",
+        message: "Which type of license would you like to use?",
+        name: "licenseInput",
+        choices: ["MIT", "other", "Apache", "GPLv2" ],
+    },
+]) 
+.then((res) => {
+    console.log(res)})
